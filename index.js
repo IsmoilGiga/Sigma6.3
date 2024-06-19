0d5a62537dcde39eeb4ac906c840ae4ee3a1bf1a
IsmoilGiga <ismoilagzamovman@gmail.com>
2024-06-19 11:52:46 +0500

    Sigma6.3

---
+++ index.js
@@ -0,0 +1,17 @@
+let input = document.querySelector("input");
+let button = document.querySelector("button");
+let p = document.querySelector("p");
+
+p.textContent = localStorage.getItem("ism");
+
+button.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    let input2 = input.value;
+
+    localStorage.setItem("ism", input2);
+
+    p.textContent = localStorage.getItem("ism");
+
+    input.value();
+});
